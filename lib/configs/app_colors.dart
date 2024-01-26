5a80ae8025daa7f4c64163aa4170b8fab63f3935
Le Tri <leminhtri151102@gmail.com>
2024-01-26 16:11:59 +0700

    feat: add configs

---
+++ lib/configs/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryPur = Color.fromRGBO(103, 80, 164, 1);
+  // static const Color primaryPur = Color.fromRGBO(130, 64, 192, 1);
+}
